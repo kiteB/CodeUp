638c823f39d3d6141ee71d3948c90b063aa41272
kiteB <69155170+kiteB@users.noreply.github.com>
2020-12-17 21:23:51 +0900

    입력받아 부분별로 출력하기

---
+++ 01_Basic/21_IO13(1023).py
@@ -0,0 +1,3 @@
+# 실수 1개 입력받아 정수 부분과 실수 부분으로 나누어 출력
+a, b = input().split('.')
+print(int(a), int(b), sep='\n')
---
+++ 01_Basic/22_IO15(1025).py
@@ -0,0 +1,8 @@
+# 정수 1개 입력받아 각 자리별로 나누어 출력하기
+a = input()
+
+print("["+str(int(a[0])*10000)+"]")
+print("["+str(int(a[1])*1000)+"]")
+print("["+str(int(a[2])*100)+"]")
+print("["+str(int(a[3])*10)+"]")
+print("["+str(int(a[4]))+"]")
