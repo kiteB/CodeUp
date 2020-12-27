f3ab9243d5170bf925b28665dbae725fa3f50aed
kiteB <69155170+kiteB@users.noreply.github.com>
2020-12-27 17:19:28 +0900

    비트단위논리연산

---
+++ 01_Basic/59_Bit_Logic02(1060).py
@@ -0,0 +1,4 @@
+# 입력된 정수 두 개를 비트단위로 and 연산한 후 그 결과를 정수로 출력하기
+# 비트단위(bitwise)연산자 & 사용하기! (and, ampersand)
+a, b = map(int, input().split())
+print(a&b)
---
+++ 01_Basic/60_Bit_Logic03(1061).py
@@ -0,0 +1,4 @@
+# 입력된 정수 두 개를 비트단위로 or 연산한 후, 그 결과를 정수로 출력하기
+# 비트단위 연산자 | (or, vertical bar) 사용하기
+a, b = map(int, input().split())
+print(a | b)
